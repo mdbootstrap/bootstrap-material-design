6235dbfa650aa90847c73d18f0f3036461e2e75a
unknown <a.jakubowski@mdbootstrap.com>
2020-12-29 10:56:00 +0100

    0.1.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,13 +18,6 @@ module.exports = function(grunt) {
             }
         },
 
-        /*githooks: {
-            all: {
-                // Will run the jshint and test:unit tasks at every commit
-                "pre-commit": "less autoprefixer cssmin uglify copy newer:jshint jasmine:scripts"
-            }
-        }, */
-
         sass: {
             compilesass: {
                 files: {
@@ -58,6 +51,14 @@ module.exports = function(grunt) {
             }
         },
 
+        clean: {
+            css: [
+                "dist/css/material.css",
+                "dist/css/material-wfont.css",
+                "dist/css/ripples.css"
+            ]
+        },
+
         uglify: {
             minifyjs: {
                 files: {
@@ -68,16 +69,10 @@ module.exports = function(grunt) {
         },
 
         copy: {
-            tpl: {
-                expand: true,
-                cwd: "dist/",
-                src: "**",
-                dest: "template/material/"
-            },
             distjs: {
                 expand: true,
                 cwd: "scripts/",
-                src: "**",
+                src: "**.min.js",
                 dest: "dist/js/",
                 flatten: true,
                 filter: "isFile"
@@ -177,9 +172,9 @@ module.exports = function(grunt) {
 
     });
 
-    grunt.registerTask("default", ["less", "autoprefixer", "cssmin", "uglify", "copy"]);
+    grunt.registerTask("default", ["less", "autoprefixer", "cssmin", "uglify", "clean", "copy"]);
 
-    grunt.registerTask("scss", ["sass", "autoprefixer", "cssmin", "uglify", "copy"]);
+    grunt.registerTask("scss", ["sass", "autoprefixer", "cssmin", "uglify", "clean", "copy"]);
 
     grunt.registerTask("build", function(target) {
         var buildType = "default";
